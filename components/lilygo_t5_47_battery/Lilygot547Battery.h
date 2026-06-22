c77f7279dc8a2eb09c872b520b6e296ad3f88427
berckvensgert-alt <berckvensgert@gmail.com>
2026-06-22 19:48:34 +0200

    Update Lilygot547Battery.h

--- components/lilygo_t5_47_battery/Lilygot547Battery.h
+++ components/lilygo_t5_47_battery/Lilygot547Battery.h
@@ -2,14 +2,9 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/core/hal.h"
-
-// Note: Using legacy ADC driver to maintain compatibility with epdiy library
-// which also uses the legacy driver. Mixing new and legacy drivers causes conflicts.
-// DO NOT use Arduino's analogRead() - it uses the new ADC driver internally
-// and conflicts with the legacy driver used by epdiy.
-#include <driver/adc.h>
-#include "esp_adc_cal.h"
-
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 #include "epdiy.h"
 
 namespace esphome {
@@ -18,16 +13,18 @@ namespace lilygo_t5_47_battery {
 class Lilygot547Battery : public PollingComponent {
  public:
   sensor::Sensor *voltage{nullptr};
-
   void setup() override;
   void update() override;
-
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage = voltage_sensor; }
 
  protected:
-  int vref_{1100};
+  adc_oneshot_unit_handle_t adc_handle_{nullptr};
+  adc_cali_handle_t cali_handle_{nullptr};
+  bool calibrated_{false};
+
   void update_battery_voltage_();
   void calibrate_adc_();
 };
+
 }  // namespace lilygo_t5_47_battery
 }  // namespace esphome
