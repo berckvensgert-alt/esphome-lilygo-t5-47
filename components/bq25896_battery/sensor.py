d9e76e5180e9228a38d274ca36e449f281b1a31c
berckvensgert-alt <berckvensgert@gmail.com>
2026-06-23 11:01:09 +0200

    Update sensor.py

--- components/bq25896_battery/sensor.py
+++ components/bq25896_battery/sensor.py
@@ -1,28 +1,22 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor, text_sensor, i2c
+from esphome.components import sensor
 from esphome.const import (
-    CONF_ID,
     CONF_VOLTAGE,
     UNIT_VOLT,
     UNIT_PERCENT,
     DEVICE_CLASS_VOLTAGE,
     DEVICE_CLASS_BATTERY,
     STATE_CLASS_MEASUREMENT,
-    ICON_BATTERY,
 )
+from . import BQ25896Battery, bq25896_ns
 
+CONF_BQ25896_BATTERY_ID = "bq25896_battery_id"
 CONF_PERCENTAGE = "percentage"
-CONF_STATUS = "status"
-
-bq25896_ns = cg.esphome_ns.namespace("bq25896_battery")
-BQ25896Battery = bq25896_ns.class_(
-    "BQ25896Battery", cg.PollingComponent, i2c.I2CDevice
-)
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(BQ25896Battery),
+        cv.GenerateID(CONF_BQ25896_BATTERY_ID): cv.use_id(BQ25896Battery),
         cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
             unit_of_measurement=UNIT_VOLT,
             accuracy_decimals=2,
@@ -35,17 +29,12 @@ CONFIG_SCHEMA = cv.Schema(
             device_class=DEVICE_CLASS_BATTERY,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
-        cv.Optional(CONF_STATUS): text_sensor.text_sensor_schema(
-            icon=ICON_BATTERY,
-        ),
     }
-).extend(cv.polling_component_schema("5s")).extend(i2c.i2c_device_schema(0x6B))
+)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
+    var = await cg.get_variable(config[CONF_BQ25896_BATTERY_ID])
 
     if CONF_VOLTAGE in config:
         sens = await sensor.new_sensor(config[CONF_VOLTAGE])
@@ -54,7 +43,3 @@ async def to_code(config):
     if CONF_PERCENTAGE in config:
         sens = await sensor.new_sensor(config[CONF_PERCENTAGE])
         cg.add(var.set_percentage_sensor(sens))
-
-    if CONF_STATUS in config:
-        sens = await text_sensor.new_text_sensor(config[CONF_STATUS])
-        cg.add(var.set_status_sensor(sens))
