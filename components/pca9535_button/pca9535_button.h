a806cc2ce78fff9d80a5b0da984726f1aff689e1
berckvensgert-alt <berckvensgert@gmail.com>
2026-06-23 11:33:43 +0200

    Update pca9535_button.h

--- components/pca9535_button/pca9535_button.h
+++ components/pca9535_button/pca9535_button.h
@@ -6,10 +6,15 @@
 namespace esphome {
 namespace pca9535_button {
 
-class PCA9535Button : public PollingComponent, public i2c::I2CDevice, public binary_sensor::BinarySensor {
+class PCA9535Button : public PollingComponent, public i2c::I2CDevice {
  public:
   void setup() override;
   void update() override;
+
+  void set_binary_sensor(binary_sensor::BinarySensor *s) { binary_sensor_ = s; }
+
+ protected:
+  binary_sensor::BinarySensor *binary_sensor_{nullptr};
 };
 
 }  // namespace pca9535_button
