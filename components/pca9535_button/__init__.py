e95ce2ddfa8051a0136f4599dbe33ff6e3822cb5
berckvensgert-alt <berckvensgert@gmail.com>
2026-06-23 11:32:49 +0200

    Update __init__.py

--- components/pca9535_button/__init__.py
+++ components/pca9535_button/__init__.py
@@ -1,27 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor, i2c
-from esphome.const import CONF_ID
+from esphome.components import i2c
 
 CODEOWNERS = ["@you"]
 DEPENDENCIES = ["i2c"]
+MULTI_CONF = True
 
 pca9535_button_ns = cg.esphome_ns.namespace("pca9535_button")
 PCA9535Button = pca9535_button_ns.class_(
-    "PCA9535Button",
-    cg.PollingComponent,
-    i2c.I2CDevice,
-    binary_sensor.BinarySensor,
+    "PCA9535Button", cg.PollingComponent, i2c.I2CDevice
 )
 
 CONFIG_SCHEMA = (
-    binary_sensor.binary_sensor_schema(PCA9535Button)
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(PCA9535Button),
+        }
+    )
     .extend(cv.polling_component_schema("200ms"))
     .extend(i2c.i2c_device_schema(0x20))
 )
 
 
 async def to_code(config):
-    var = await binary_sensor.new_binary_sensor(config)
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
